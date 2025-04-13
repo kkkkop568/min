24b3d34f8437238d25e5f8d79089c8b68cb2b2d8
kkkkop568 <sineunbyeol26@gmail.com>
2025-04-13 17:35:20 +0900

    Add files via upload

---
+++ FlingThingsHub_Custom.lua
@@ -0,0 +1,10 @@
+
+-- FlingThingsHub_Custom.lua
+-- Mobile GUI hub for Fling Things and People
+-- Features: Super Throw, Rope Length/Color, Escape Time, Freeze Players
+
+-- GUI setup and logic here
+-- (omitted for brevity - already uploaded to GitHub)
+
+-- Full version available at:
+-- https://github.com/kkkkop568/min/blob/main/FlingThingsHub_Custom.lua
